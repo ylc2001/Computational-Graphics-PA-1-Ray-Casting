3e788114cb7312fbccb645e64b590603819e328e
杨力忱 <2020011007@secoder.net>
2022-04-02 23:39:52 +0800

    added .gitignore, 实现了球面的求交算法（但遇到编译错误）

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -27,19 +27,50 @@ public:
     bool intersect(const Ray &r, Hit &h, float tmin) override
     {
         //
-        //Vector3f Ro = r.getOrigin();
-        //Vector3f Rd = r.getDirection();
-        //Vector3f Pc = this->_center;
-        //float b = Vector3f::dot(Rd, Ro - Pc);
-        //float c = Vector3f::dot(Ro - Pc, Ro - Pc) - (_radius * _radius);
-        //if (b * b - c >= 0)
-        //{
-        //    float t_1 = -b + sqrt(b * b - c);
-        //}
-        //else
-        //{
-        //    return false;
-        //}
+        Vector3f Ro = r.getOrigin();
+        Vector3f Rd = r.getDirection();
+        Vector3f Pc = this->_center;
+        float b = Vector3f::dot(Rd, Ro - Pc);
+        float c = Vector3f::dot(Ro - Pc, Ro - Pc) - (_radius * _radius);
+        float result_t;
+        if (b * b - c >= 0)
+        {
+            float t_1 = -b + sqrt(b * b - c);
+            float t_2 = -b - sqrt(b * b - c);
+            if (t_1 == t_2)
+            {
+                if (t_1 < tmin)
+                    return false;
+                else
+                {
+                    result_t = t_1;
+                    // return true;
+                }
+            }
+            else
+            {
+                if (max(t_1, t_2) < tmin)
+                    return false;
+                else if (min(t_1, t_2) < tmin)
+                {
+                    result_t = max(t_1, t_2);
+                    // return true;
+                }
+                else
+                {
+                    result_t = min(t_1, t_2);
+                    // return true;
+                }
+            }
+            Vector3f n = Ro + (result_t * Rd) - Pc; //法向量
+            n.normalize();
+            h.set(result_t, this->material, n);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 protected:
