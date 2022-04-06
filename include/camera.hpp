6094f19a8d6e0555b87774e5f417e825eb64b81f
杨力忱 <2020011007@secoder.net>
2022-04-06 10:24:27 +0800

    Perspective camera done

--- include/camera.hpp
+++ include/camera.hpp
@@ -6,10 +6,11 @@
 #include <float.h>
 #include <cmath>
 
-
-class Camera {
+class Camera
+{
 public:
-    Camera(const Vector3f &center, const Vector3f &direction, const Vector3f &up, int imgW, int imgH) {
+    Camera(const Vector3f &center, const Vector3f &direction, const Vector3f &up, int imgW, int imgH)
+    {
         this->center = center;
         this->direction = direction.normalized();
         this->horizontal = Vector3f::cross(this->direction, up).normalized();
@@ -25,30 +26,47 @@ public:
     int getWidth() const { return width; }
     int getHeight() const { return height; }
 
-protected:
     // Extrinsic parameters
     Vector3f center;
     Vector3f direction;
     Vector3f up;
     Vector3f horizontal;
+
+protected:
     // Intrinsic parameters
     int width;
     int height;
 };
 
-// TODO: Implement Perspective camera
+// --V--TODO: Implement Perspective camera
 // You can add new functions or variables whenever needed.
-class PerspectiveCamera : public Camera {
+class PerspectiveCamera : public Camera
+{
 
 public:
     PerspectiveCamera(const Vector3f &center, const Vector3f &direction,
-            const Vector3f &up, int imgW, int imgH, float angle) : Camera(center, direction, up, imgW, imgH) {
+                      const Vector3f &up, int imgW, int imgH, float angle) : Camera(center, direction, up, imgW, imgH)
+    {
         // angle is in radian.
+        this->angle = angle;
     }
 
-    Ray generateRay(const Vector2f &point) override {
-        // 
+    Ray generateRay(const Vector2f &point) override
+    {
+        // Fx = Fy
+        float Fx = this->getWidth() / (2 * tan(angle / 2));
+        float Fy = this->getHeight() / (2 * tan(angle / 2));
+        float u = point.x();
+        float v = point.y();
+        int cx = this->getWidth() / 2;
+        int cy = this->getHeight() / 2;
+        // Vector3f direction = Vector3f((u - cx) / Fx, (cy - v) / Fy, 1);
+        Vector3f Ray_direction = ((u - cx) / Fx) * this->horizontal + ((cy - v) / Fy) * this->up + this->direction;
+        Ray_direction.normalize();
+        return Ray(this->center, Ray_direction);
     }
+
+    float angle;
 };
 
-#endif //CAMERA_H
+#endif // CAMERA_H
--- src/main.cpp
+++ src/main.cpp
@@ -38,42 +38,43 @@ int main(int argc, char *argv[])
     // pixel in your output image.
 
     SceneParser sceneParser(argv[1]);
-    Camera *camera = sceneParser.getCamera();
+    PerspectiveCamera *camera = (PerspectiveCamera*)sceneParser.getCamera();
     Image renderedImage(camera->getWidth(), camera->getHeight());
 
     for (int x = 0; x < camera->getWidth(); ++x)
     {
         for (int y = 0; y < camera->getHeight(); ++y)
         {
-            //// 计算当前像素(x,y)处相机出射光线camRay
-            //Ray camRay = camera->generateRay(Vector2f(x, y));
-            //Group *baseGroup = sceneParser.getGroup();
-            //Hit hit;
-            //// 判断camRay是否和场景有交点，并返回最近交点的数据，存储在hit中
-            //bool isIntersect = baseGroup->intersect(camRay, hit, 0);
-            //if (isIntersect)
-            //{
-            //    Vector3f finalColor = Vector3f::ZERO;
-            //    // 找到交点之后，累加来自所有光源的光强影响
-            //    for (int li = 0; li < sceneParser.getNumLights(); ++li)
-            //    {
-            //        Light *light = sceneParser.getLight(li);
-            //        Vector3f L, lightColor;
-            //        // 获得光照强度
-            //        light->getIllumination(camRay.pointAtParameter(hit.getT()), L, lightColor);
-            //        // 计算局部光强
-            //        finalColor += hit.getMaterial()->Shade(camRay, hit, L, lightColor);
-            //    }
-            //    renderedImage.SetPixel(x, y, finalColor);
-            //}
-            //else
-            //{
-            //    // 不存在交点，返回背景色
-            //    renderedImage.SetPixel(x, y, sceneParser.getBackgroundColor());
-            //}
-            renderedImage.SetPixel(x, y, Vector3f((float)x/200, (float)y/200, 0.5));
+            // 计算当前像素(x,y)处相机出射光线camRay
+            Ray camRay = camera->generateRay(Vector2f(x, y));
+            Group *baseGroup = sceneParser.getGroup();
+            Hit hit;
+            // 判断camRay是否和场景有交点，并返回最近交点的数据，存储在hit中
+            bool isIntersect = baseGroup->intersect(camRay, hit, 0);
+            if (isIntersect)
+            {
+                Vector3f finalColor = Vector3f::ZERO;
+                // 找到交点之后，累加来自所有光源的光强影响
+                for (int li = 0; li < sceneParser.getNumLights(); ++li)
+                {
+                    Light *light = sceneParser.getLight(li);
+                    Vector3f L, lightColor;
+                    // 获得光照强度
+                    light->getIllumination(camRay.pointAtParameter(hit.getT()), L, lightColor);
+                    // 计算局部光强
+                    finalColor += hit.getMaterial()->Shade(camRay, hit, L, lightColor);
+                }
+                renderedImage.SetPixel(x, y, finalColor);
+            }
+            else
+            {
+                // 不存在交点，返回背景色
+                renderedImage.SetPixel(x, y, sceneParser.getBackgroundColor());
+            }
+            //renderedImage.SetPixel(x, y, Vector3f((float)x/200, (float)y/200, 0.5));
         }
     }
+    cout << "Render complete. saving to ... " << argv[2] << endl;
     renderedImage.SaveImage(argv[2]);
     cout << "Hello! Computer Graphics!" << endl;
     return 0;
